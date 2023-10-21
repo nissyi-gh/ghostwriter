d09e6b3faf3def1bf5b865e305b3ff4baf8a283c
yuta.onishi <rad6242@icloud.com>
2023-10-21 19:24:00 +0900

    アイコンボタンを埋め込むjsを追加
    プロンプトの画面でクリックできるようにします

---
+++ content/chatgpt/content.js
@@ -0,0 +1,9 @@
+const ghostWriterIconImage = document.createElement('img');
+ghostWriterIconImage.src = chrome.runtime.getURL('images/ghost_writer_128.png');
+
+const ghostWriterButton = document.createElement('button');
+ghostWriterButton.appendChild(ghostWriterIconImage);
+
+// プロンプト送信ボタンの前にアイコンを挿入
+const buttonElement = document.querySelector('[data-testid="send-button"]');
+buttonElement.parentNode.insertBefore(ghostWriterButton, buttonElement);
