759bc3b591dd41cbfd46fa07e6a1653db454040b
yuta.onishi <rad6242@icloud.com>
2023-10-21 20:02:00 +0900

    プロンプト入力フォームの枠内にアイコンを表示できるようにする
    入力時に使いやすそうな位置に配置します

--- content/chatgpt/content.js
+++ content/chatgpt/content.js
@@ -2,8 +2,9 @@ const ghostWriterIconImage = document.createElement('img');
 ghostWriterIconImage.src = chrome.runtime.getURL('images/ghost_writer_128.png');
 
 const ghostWriterButton = document.createElement('button');
-ghostWriterButton.appendChild(ghostWriterIconImage);
+ghostWriterButton.className = 'ghost-writer-button';
 
+ghostWriterButton.appendChild(ghostWriterIconImage);
 // プロンプト送信ボタンの前にアイコンを挿入
 const buttonElement = document.querySelector('[data-testid="send-button"]');
 buttonElement.parentNode.insertBefore(ghostWriterButton, buttonElement);
