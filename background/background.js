e91358537e40cc6c03ca6921c4c9591f67c70fe2
yuta.onishi <rad6242@icloud.com>
2023-10-22 18:25:45 +0900

    DBの初期化及びサンプルプロンプトの作成を実装
    検証しやすいように毎度dbを削除しています

--- background/background.js
+++ background/background.js
@@ -1,1 +1,47 @@
-console.log('test loading background.js')
+let db;
+
+function initDB() {
+  const request = indexedDB.open("ghostWriterDB");
+
+  request.onsuccess = function(event) {
+    db = event.target.result;
+    console.log("ghostWriterDB initialized:", event);
+  };
+
+  request.onerror = function(event) {
+    console.log("Error initializing ghostWriterDB:", event);
+  };
+
+  request.onupgradeneeded = function(event) {
+    db = event.target.result;
+
+    if (!db.objectStoreNames.contains('prompts')) {
+      const promptsDb = db.createObjectStore("prompts", { keyPath: "id", autoIncrement: true });
+      promptsDb.add({
+        title: "サンプルのプロンプト 優秀なWebエンジニア",
+        prompt: "## 前提\nあなたは優秀なWebエンジニアです。以下の質問に回答してください。\n\n## 質問\n（ここに質問を書く）\n\n## 回答\n回答は必ず日本語で答えてください。"
+      });
+      console.log("ghostWriterDB's prompts object store created, and sample prompt added.");
+    }
+  };
+}
+
+function deleteDB(callback) {
+  const request = indexedDB.deleteDatabase("ghostWriterDB");
+
+  request.onsuccess = function(event) {
+    console.log("Database deleted successfully.");
+    callback();
+  };
+
+  request.onerror = function(event) {
+    console.log("Error deleting database:", event);
+  };
+
+  request.onblocked = function(event) {
+    console.log("Delete operation blocked. Close any open instances of the database and try again.");
+  };
+}
+
+// 初期化の実験を開始
+deleteDB(initDB);
